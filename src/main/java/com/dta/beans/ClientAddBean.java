a753ea3d64c5eb3cf977a05ccf30897668e44416
Gael Calmettes <calmettesgael@gmail.com>
2015-05-04 14:31:13 +0200

    add package metier + update

--- src/main/java/com/dta/beans/ClientAddBean.java
+++ src/main/java/com/dta/beans/ClientAddBean.java
@@ -1,58 +1,102 @@
 package com.dta.beans;
 
 import javax.faces.bean.ManagedBean;
+import javax.persistence.Column;
+
+import com.dta.entities.Utilisateur;
 
 @ManagedBean(name="addClient")
 public class ClientAddBean {
 	
-	String name;
-	String firstName;
-	String passWord;
-	int age;
-	String address;
+	Utilisateur utilisateur;
 	
+	private String email;
+	private int fax;
+	private String login;
+	private String nom;
+	private String password;
+	private String prenom;
+	private int telephone;
+	private String titre;
+	private String typeUtil;
+	private String address;
+
+	
+
 	@Override
 	public String toString() {
-		return "ManagedClient name=" + name 
-				+ ", firstName=" + firstName
-				+ ", passWord=" + passWord 
-				+ ", age=" + age 
-				+ ", address=" + address ;
+		return "ClientAddBean [email=" + email + ", fax=" + fax + ", login="
+				+ login + ", nom=" + nom + ", password=" + password
+				+ ", prenom=" + prenom + ", telephone=" + telephone
+				+ ", titre=" + titre + ", typeUtil=" + typeUtil + "]";
 	}
 
 
 	public void save(){
 		System.out.println(toString());
 	}
-	
-	
-	public String getName() {
-		return name;
+
+
+	public String getEmail() {
+		return email;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	public String getFirstName() {
-		return firstName;
+	public int getFax() {
+		return fax;
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setFax(int fax) {
+		this.fax = fax;
 	}
-	public String getPassWord() {
-		return passWord;
+	public String getLogin() {
+		return login;
 	}
-	public void setPassWord(String passWord) {
-		this.passWord = passWord;
+	public void setLogin(String login) {
+		this.login = login;
 	}
-	public int getAge() {
-		return age;
+	public String getNom() {
+		return nom;
 	}
-	public void setAge(int age) {
-		this.age = age;
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getTypeUtil() {
+		return typeUtil;
+	}
+	public void setTypeUtil(String typeUtil) {
+		this.typeUtil = typeUtil;
+	}
+
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
