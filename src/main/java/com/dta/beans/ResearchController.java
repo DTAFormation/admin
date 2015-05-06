2c95fa132881a5af8d3e6323143c446ba55d48ef
Julien Paget <julien.paget@ymail.com>
2015-05-06 17:12:15 +0200

    usr story 110 #16 identification admin / modo

--- src/main/java/com/dta/beans/ResearchController.java
+++ src/main/java/com/dta/beans/ResearchController.java
@@ -1,13 +1,15 @@
 package com.dta.beans;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import com.dta.entities.Article;
-import com.dta.entities.Produit;
 import com.dta.entities.Utilisateur;
 import com.dta.metier.SearchArticle;
 import com.dta.metier.SearchProduit;
@@ -40,8 +42,8 @@ public class ResearchController {
 	private SearchUtilisateur searchUtilisateur;
 
 	private List<Article> products;
-	private List<Utilisateur> users; 
-	
+	private List<Utilisateur> users;
+
 	/*
 	 * 
 	 * Constructors
@@ -83,6 +85,12 @@ public class ResearchController {
 	 * Controller Methods
 	 * 
 	 */
+	
+	public void logout() throws IOException{
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		FacesContext.getCurrentInstance().getExternalContext().redirect("authentification.xhtml");
+		
+	}
 	
 	public void submitResearchArticle() {
 
