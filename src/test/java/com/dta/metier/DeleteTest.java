afa94eda529710f73fd8d7ab7c9e3aed2a073116
lrt <laurent.riviere09@gmail.com>
2015-05-07 12:04:24 +0200

    User Story 540: en tant qu'admin/mod, je peux supprimer un produit #18 -tests

--- src/test/java/com/dta/metier/DeleteTest.java
+++ src/test/java/com/dta/metier/DeleteTest.java
@@ -1,6 +1,7 @@
 package com.dta.metier;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,32 +12,68 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.dta.beans.ArticleBean;
+import com.dta.beans.UtilisateurBean;
 import com.dta.entities.Article;
 import com.dta.entities.Produit;
+import com.dta.entities.Utilisateur;
 
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DeleteTest {
-	
-	private DeleteArticle deleteArticle;
 
-	private static final Logger LOG = LoggerFactory.getLogger(DeleteArticle.class);	
-	
+	private DeleteArticle deleteArticleEJB;
+	private DeleteUtilisateur deleteUtilisateurEJB;
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(DeleteArticle.class);
+
 	@Mock private EntityManager em;
 	@Mock private ArticleBean articleBean;
-	
+	@Mock private UtilisateurBean utilisateurBean;
+
 	@Before
 	public void setUp() {
-		deleteArticle = new DeleteArticle();
-		deleteArticle.setEm(em);
+		deleteArticleEJB = new DeleteArticle();
+		deleteArticleEJB.setEm(em);
+		deleteUtilisateurEJB = new DeleteUtilisateur();
+		deleteUtilisateurEJB.setEm(em);
+	}
+
+	@Test
+	public void testDeleteArticle() {
+		LOG.info("Création d'un produit P");
+		Produit produit = new Produit("description", "nomProduit");
+		LOG.info("Création d'un article A dans les produits P");
+		Article article = new Article("nomArticle", 18.2f, produit, 12);
+
+		when(em.find(Article.class, 0)).thenReturn(article);
+		doNothing().when(em).remove(article);
+		deleteArticleEJB.setEm(em);
+		deleteArticleEJB.delete(0);
+		verify(em).remove(article);
 	}
 	
 	@Test
-	public void testDeleteArticle(){
-		Produit produit = new Produit("description", "nomProduit");
-		Article article = new Article("nomArticle", 18.2f, produit, 12);
-		
+	public void testDeleteUtilisateur() {
+		LOG.info("Création d'un utilisateur");
+		Utilisateur utilisateur = new Utilisateur(
+				"email@dta.com",
+				1,
+				"login",
+				"nom",
+				"password",
+				"prenom",
+				1,
+				"titre",
+				"m",
+				null);
+				
+		when(em.find(Utilisateur.class, 0)).thenReturn(utilisateur);
+		doNothing().when(em).remove(utilisateur);
+		deleteUtilisateurEJB.setEm(em);
+		deleteUtilisateurEJB.delete(0);
+		verify(em).remove(utilisateur);
 	}
 
 }
