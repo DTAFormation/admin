e3930f36cfe4cfd050933f857ce15349a078cf0d
Sebastien Boureau <boureau.sebastien@gmail.com>
2015-05-12 16:59:45 +0200

    Bug fix sur la vue de modification d'article*

--- src/main/java/com/dta/beans/DetailProduitBean.java
+++ src/main/java/com/dta/beans/DetailProduitBean.java
@@ -5,37 +5,39 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import com.dta.entities.Article;
-
-import com.dta.metier.SearchArticleEJB;
 import com.dta.metier.ModifyArticleEJB;
-
+import com.dta.metier.SearchArticleEJB;
 
 @ManagedBean
 @ViewScoped
-public class DetailProduitBean{
-	
+public class DetailProduitBean {
+
 	private Article article;
-		
+
 	@EJB
-	private ModifyArticleEJB ejb;	
-	
+	private ModifyArticleEJB ejb;
+
 	@EJB
 	private SearchArticleEJB searchArticle;
-			
-	public Article showDetailArticle(int id) {		
+
+	public Article showDetailArticle(int id) {
+		return searchArticle.findById(id).get(0);
+	}
+
+	public void initArticle(int id) {
 		article = searchArticle.findById(id).get(0);
-		return article;
-    }
-	
-	public void saveDetailArticle(){
+	}
+
+	public void saveDetailArticle() {
 		ejb.update(article);
 	}
 
 	public Article getArticle() {
+		System.out.println("Article: " + article);
 		return article;
 	}
 
 	public void setArticle(Article article) {
 		this.article = article;
-	}	
+	}
 }
