7768168b5cd7ee74f70e6aec92711f5da1bbd8f0
Florent Berthelot <florent.berthelot@sfr.fr>
2015-05-18 12:57:38 +0200

    Update sql

--- ecommercedb.sql
+++ ecommercedb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 15 Mai 2015 à 11:34
+-- Généré le :  Lun 18 Mai 2015 à 12:54
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -85,8 +85,11 @@ INSERT INTO `adresses_utilisateur` (`utilisateur_utilisateur_id`, `adresse_id`) 
 
 CREATE TABLE IF NOT EXISTS `article` (
   `article_id` int(11) NOT NULL,
-  `nom` varchar(255) DEFAULT NULL,
+  `image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `nom` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `nb_raters` int(11) DEFAULT NULL,
   `prix` float DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
   `stock` int(11) DEFAULT NULL,
   `produit_produit_id` int(11) DEFAULT NULL,
   `version` bigint(20) NOT NULL,
@@ -99,13 +102,17 @@ CREATE TABLE IF NOT EXISTS `article` (
 -- Contenu de la table `article`
 --
 
-INSERT INTO `article` (`article_id`, `nom`, `prix`, `stock`, `produit_produit_id`, `version`) VALUES
-(1, 'ballon', 15, 12, 1, 0),
-(2, 'crampons', 8, 43, 1, 0),
-(3, 'slip de bain', 10, 19, 2, 0),
-(4, 'Pecheresse', 3.2, 150, 3, 0),
-(5, 'Bécasse Cerise', 3.4, 100, 3, 0),
-(6, 'Verte du montblanc', 5, 50, 3, 0);
+INSERT INTO `article` (`article_id`, `image`, `nom`, `nb_raters`, `prix`, `rating`, `stock`, `produit_produit_id`, `version`) VALUES
+(1, 'http://i.imgur.com/4O86daR.jpg', 'ballon de foot', 0, 10, 0, 25, 1, 0),
+(2, 'http://i.imgur.com/kG47bs8.jpg', 'chaussures de foot', 0, 500, 0, 2000, 1, 0),
+(3, 'http://i.imgur.com/iDWMUTr.jpg', 'ballon de basket', 0, 15, 0, 30, 2, 0),
+(4, 'http://i.imgur.com/z3rOjE9.jpg', 'panier de basket', 0, 100, 0, 30, 2, 0),
+(5, 'http://i.imgur.com/NUOJOdC.jpg', 'concombre', 0, 2.35, 0, 10, 3, 0),
+(6, 'http://i.imgur.com/lhxWEQ3.jpg', 'courgette', 0, 59.99, 0, 5000, 3, 0),
+(7, 'http://i.imgur.com/vBRzDhq.jpg', 'pomme de terre', 0, 2.65, 0, 1000, 4, 0),
+(8, 'http://i.imgur.com/XhUhqXX.jpg', 'carotte', 0, 3.05, 0, 60, 4, 0),
+(9, 'http://i.imgur.com/BT2FVWT.jpg', 'banane', 0, 4.6, 0, 35, 5, 0),
+(10, 'http://i.imgur.com/MQfZPWa.png', 'tomate', 0, 4.7, 0, 25, 5, 0);
 
 -- --------------------------------------------------------
 
@@ -127,8 +134,8 @@ CREATE TABLE IF NOT EXISTS `catalogue` (
 --
 
 INSERT INTO `catalogue` (`catalogue_id`, `description`, `nom`, `version`) VALUES
-(1, 'articles de sport', 'sport', 0),
-(2, 'Boissons', 'Boissons', 0);
+(1, 'catalogue de sport', 'sport', 0),
+(2, 'catalogue de primeurs', 'primeurs', 0);
 
 -- --------------------------------------------------------
 
@@ -143,6 +150,7 @@ CREATE TABLE IF NOT EXISTS `commande` (
   `num_cartecredit` varchar(255) DEFAULT NULL,
   `type_cartecredit` varchar(255) DEFAULT NULL,
   `version` int(11) NOT NULL,
+  `validate` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`commande_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -150,12 +158,12 @@ CREATE TABLE IF NOT EXISTS `commande` (
 -- Contenu de la table `commande`
 --
 
-INSERT INTO `commande` (`commande_id`, `date_commande`, `date_expiration_cartecredit`, `num_cartecredit`, `type_cartecredit`, `version`) VALUES
-(1, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '6531', 'bancaire', 0),
-(2, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '9865', 'bancaire', 0),
-(3, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '1356', 'bancaire', 0),
-(4, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '9647', 'bancaire', 0),
-(5, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '7653', 'bancaire', 0);
+INSERT INTO `commande` (`commande_id`, `date_commande`, `date_expiration_cartecredit`, `num_cartecredit`, `type_cartecredit`, `version`, `validate`) VALUES
+(1, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '6531', 'bancaire', 0, 1),
+(2, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '9865', 'bancaire', 0, 1),
+(3, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '1356', 'bancaire', 0, 1),
+(4, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '9647', 'bancaire', 0, 1),
+(5, '2015-05-05 14:43:57', '2015-05-05 14:43:57', '7653', 'bancaire', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -243,7 +251,7 @@ CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
 --
 
 INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-  (5);
+(5);
 
 -- --------------------------------------------------------
 
@@ -256,19 +264,21 @@ CREATE TABLE IF NOT EXISTS `lignecommande` (
   `quantite` int(11) NOT NULL,
   `article_article_id` int(11) DEFAULT NULL,
   `version` bigint(20) NOT NULL,
+  `commande_commande_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`ligneCommandeId`),
-  KEY `FK_b62ftsfx7klnhqpa8ukpr4cxl` (`article_article_id`)
+  KEY `FK_b62ftsfx7klnhqpa8ukpr4cxl` (`article_article_id`),
+  KEY `FK_028c6f159ebf4df389967c3ae85` (`commande_commande_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `lignecommande`
 --
 
-INSERT INTO `lignecommande` (`ligneCommandeId`, `quantite`, `article_article_id`, `version`) VALUES
-(1, 5, 1, 0),
-(2, 25, 2, 0),
-(3, 14, 1, 0),
-(4, 12, 3, 0);
+INSERT INTO `lignecommande` (`ligneCommandeId`, `quantite`, `article_article_id`, `version`, `commande_commande_id`) VALUES
+(1, 5, 1, 0, 1),
+(2, 25, 2, 0, 1),
+(3, 14, 1, 0, 2),
+(4, 12, 3, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -292,9 +302,11 @@ CREATE TABLE IF NOT EXISTS `produit` (
 --
 
 INSERT INTO `produit` (`produit_id`, `description`, `nom`, `catalogue_catalogue_id`, `version`) VALUES
-(1, 'articles de foot', 'football', 1, 0),
-(2, 'articles de natation', 'natation', 1, 0),
-(3, 'Bières', 'Bière', 2, 0);
+(1, 'liste de produits concernant le football', 'football', 1, 0),
+(2, 'liste de produits concernant le basketball', 'natation', 1, 0),
+(3, 'liste de cucurbitacés', 'cucurbitacé', 2, 0),
+(4, 'liste de tubercules', 'tubercule', 2, 0),
+(5, 'liste de fruits', 'fruits', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -314,6 +326,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `titre` varchar(255) DEFAULT NULL,
   `type_util` varchar(1) DEFAULT NULL,
   `version` bigint(20) NOT NULL,
+  `active` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`utilisateur_id`),
   UNIQUE KEY `UK_35ysk0sh9ruwixrld3nc0weut` (`email`),
   UNIQUE KEY `UK_kmw1w139mxftir6ce47jrbxac` (`login`)
@@ -323,11 +336,11 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 -- Contenu de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`utilisateur_id`, `email`, `fax`, `login`, `nom`, `password`, `prenom`, `telephone`, `titre`, `type_util`, `version`) VALUES
-(1, 'thunder@hotmail.com', 1357, 'thunder', 'abitbol', 'theclass', 'georges', 3548, 'm', 'a', 0),
-(2, 'epicness@hotmail.com', 7865, 'plop', 'dupond', 'ploplop', 'julie', 8541, 'mme', 'c', 0),
-(3, 'salameche92@hotmail.com', 9852, 'username', 'dupont', 'secret', 'bob', 5026, 'm', 'm', 0),
-(4, 'vegeta91@hotmail.com', 9025, 'pif1991', 'collins', 'genesis', 'phil', 6932, 'm', 'm', 0);
+INSERT INTO `utilisateur` (`utilisateur_id`, `email`, `fax`, `login`, `nom`, `password`, `prenom`, `telephone`, `titre`, `type_util`, `version`, `active`) VALUES
+(1, 'thunder@hotmail.com', 1357, 'thunder', 'abitbol', 'theclass', 'georges', 3548, 'm', 'a', 0, 1),
+(2, 'epicness@hotmail.com', 7865, 'plop', 'dupond', 'ploplop', 'julie', 8541, 'mme', 'c', 0, 1),
+(3, 'salameche92@hotmail.com', 9852, 'username', 'dupont', 'secret', 'bob', 5026, 'm', 'm', 0, 1),
+(4, 'vegeta91@hotmail.com', 9025, 'pif1991', 'collins', 'genesis', 'phil', 6932, 'm', 'm', 0, 1);
 
 --
 -- Contraintes pour les tables exportées
@@ -371,6 +384,7 @@ ALTER TABLE `commande_lignecommande`
 -- Contraintes pour la table `lignecommande`
 --
 ALTER TABLE `lignecommande`
+  ADD CONSTRAINT `FK_028c6f159ebf4df389967c3ae85` FOREIGN KEY (`commande_commande_id`) REFERENCES `commande` (`commande_id`),
   ADD CONSTRAINT `FK_b62ftsfx7klnhqpa8ukpr4cxl` FOREIGN KEY (`article_article_id`) REFERENCES `article` (`article_id`);
 
 --
