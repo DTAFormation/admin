51649c9632b5c73539d44039ab287d9328578cc3
Sebastien Boureau <boureau.sebastien@gmail.com>
2015-05-19 12:08:08 +0200

    Ajustements sur les vues ajout article et catalogue

--- src/main/java/com/dta/beans/AjoutArticleBean.java
+++ src/main/java/com/dta/beans/AjoutArticleBean.java
@@ -3,7 +3,9 @@ package com.dta.beans;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
 
@@ -11,29 +13,36 @@ import com.dta.entities.Article;
 import com.dta.entities.Produit;
 import com.dta.metier.AddArticleEJB;
 
-@ManagedBean(name="ajoutArticle")
+@ManagedBean(name = "ajoutArticle")
 public class AjoutArticleBean {
-	
+
 	Article article;
-	
+
 	private String nom;
 	private float prix;
 	private int stock;
 	private String image;
 	private int produitId;
 	private Produit produit;
-	
+
 	@EJB
 	private AddArticleEJB ejb;
-	
-	public void save(){
+
+	public void save() {
 		produit = ejb.getProduitById(produitId);
 		article = new Article(nom, prix, produit, stock, image);
 		ejb.save(article);
+		notifyAjout();
 		cleanAndUpdate();
 	}
-	
-	public void cleanAndUpdate() {
+
+	private void notifyAjout() {
+		FacesMessage msg = new FacesMessage("Article enregistré");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		updateElement("ajoutArticleForm:msgs");
+	}
+
+	private void cleanAndUpdate() {
 		nom = null;
 		prix = 0f;
 		stock = 0;
@@ -42,15 +51,15 @@ public class AjoutArticleBean {
 		produit = null;
 		updateElement("ajoutArticleForm");
 	}
-	
+
 	private void updateElement(String elementId) {
 		RequestContext.getCurrentInstance().update(elementId);
 	}
-	
+
 	public List<Produit> getAllProduits() {
-		return ejb.getAllProduits(); 
+		return ejb.getAllProduits();
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
@@ -67,7 +76,6 @@ public class AjoutArticleBean {
 		this.prix = prix;
 	}
 
-	
 	public int getProduitId() {
 		return produitId;
 	}
@@ -90,5 +98,5 @@ public class AjoutArticleBean {
 
 	public void setImage(String image) {
 		this.image = image;
-	}	
+	}
 }
--- src/main/java/com/dta/beans/AjoutCatalogueBean.java
+++ src/main/java/com/dta/beans/AjoutCatalogueBean.java
@@ -1,7 +1,11 @@
 package com.dta.beans;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.context.RequestContext;
 
 import com.dta.entities.Catalogue;
 import com.dta.metier.AddCatalogueEJB;
@@ -20,6 +24,24 @@ public class AjoutCatalogueBean {
 	public void save(){
 		catalogue = new Catalogue(description, nom, null);
 		ejb.save(catalogue);
+		notifyAjout();
+		cleanAndUpdate();
+	}
+	
+	private void notifyAjout() {
+		FacesMessage msg = new FacesMessage("Catalogue enregistré");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		updateElement("ajoutCatalogueForm:msgs");
+	}
+
+	private void cleanAndUpdate() {
+		nom = null;
+		description = null;
+		updateElement("ajoutCatalogueForm");
+	}
+	
+	private void updateElement(String elementId) {
+		RequestContext.getCurrentInstance().update(elementId);
 	}
 
 	public Catalogue getCatalogue() {
